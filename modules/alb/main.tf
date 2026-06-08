9106d5ed901c5544724a9527c3a374105883a3ce
Franco Quiroga <quirogafranco318@gmail.com>
2026-06-08 18:48:22 -0300

    listo sg del alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -18,6 +18,39 @@ resource "aws_lb" "main" {
 
 }
 
+#SG del ALB (tráfico HTTPS desde internet)
+resource "aws_security_group" "alb" {
+  name        = "${var.name_prefix}-sg-alb"
+  description = "ALB: acepta HTTPS desde internet, permite todo saliente"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTPS desde internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP → redirigir a HTTPS (listener de redirección)
+  ingress {
+    description = "HTTP (redirección a HTTPS)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  //CICLO DE VIDA LO DEJO???
+  lifecycle { create_before_destroy = true }
+}
+
 //tg, sg, task
 # Target Group: mango-api 
 resource "aws_lb_target_group" "api" {
