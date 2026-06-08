aba614b2aaa9b69ccab7570124819cd796bc2744
Franco Quiroga <quirogafranco318@gmail.com>
2026-06-08 18:50:00 -0300

    alb

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,7 +1,7 @@
 # modules/alb/outputs.tf
 
 output "alb_arn"              { value = aws_lb.main.arn }
-output "alb_sg_id" { value = aws_security_group.alb.id }
+output "alb_sg_id"            { value = aws_security_group.alb.id }
 output "alb_dns_name"         { value = aws_lb.main.dns_name }
 output "alb_zone_id"          { value = aws_lb.main.zone_id }
 output "api_target_group_arn" { value = aws_lb_target_group.api.arn }
